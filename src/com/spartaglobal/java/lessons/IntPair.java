1a1e94641353c835aed6b4e25792c61a392aa2c1
VioletViola <vivian.v18723@gmail.com>
2021-09-22 08:02:19 +0100

    Add IntPair class and used for referencing.

---
+++ src/com/spartaglobal/java/lessons/IntPair.java
@@ -0,0 +1,19 @@
+package com.spartaglobal.java.lessons;
+
+public class IntPair {
+    private int first;
+    private int second;
+
+    public IntPair(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public int getFirst() {
+        return first;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+}
--- src/com/spartaglobal/java/lessons/Ranges.java
+++ src/com/spartaglobal/java/lessons/Ranges.java
@@ -27,7 +27,11 @@ public class Ranges {
         // calculating sum of all ints from startInt to endInt
         int result = 0;
         if (endInt < startInt) {
-            swapIntsWhenInWrongOrder(startInt, endInt);
+            // added IntPair referencing
+            IntPair thePair = swapIntsWhenInWrongOrder(startInt, endInt);
+            // added due to IntPair getters
+            startInt = thePair.getFirst();
+            endInt = thePair.getSecond();
         }
         for (int i = startInt; i <= endInt; i++) {
             result += i;
@@ -35,11 +39,17 @@ public class Ranges {
         return result;
     }
 
-    public static void swapIntsWhenInWrongOrder(int firstNum, int secondNum) {
-        System.out.println("Before swap firstNum = " + firstNum + " secondNum = " + secondNum);
-        int tempStorage = firstNum;
-        firstNum = secondNum;
-        secondNum = tempStorage;
-        System.out.println("After swap firstNum = " + firstNum + " secondNum = " + secondNum);
+    // changed from void to IntPair type
+    public static IntPair swapIntsWhenInWrongOrder(int firstNum, int secondNum) {
+        // below added via IntPair
+        IntPair intPair = new IntPair(firstNum, secondNum);
+        return intPair;
+        /*  (the below is no longer needed in order to fix the scoping issue of not returning 0):
+            System.out.println("Before swap firstNum = " + firstNum + " secondNum = " + secondNum);
+            int tempStorage = firstNum;
+            firstNum = secondNum;
+            secondNum = tempStorage;
+            System.out.println("After swap firstNum = " + firstNum + " secondNum = " + secondNum);
+         */
     }
 }
